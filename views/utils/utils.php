f1db54aac019c496c61f0fb62413617be09e4212
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-10-10 17:48:40 +0200

    .

--- views/utils/utils.php
+++ views/utils/utils.php
@@ -1,7 +1,16 @@
 <?php
-function menuActivo($menu): bool
+function esOpcionMenuActiva($menu): bool
 {
     if ($_SERVER['REQUEST_URI'] == $menu) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function esOpcionMenuActivaEnArray(...$arrayOpciones): bool
+{
+    if (esOpcionMenuActiva($arrayOpciones[0]) || esOpcionMenuActiva($arrayOpciones[1])) {
         return true;
     } else {
         return false;
