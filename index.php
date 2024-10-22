03a6d4eb4b02c9907124eb8664319dfed234f238
Miguel Angel <sanmi22@informatica.iesvalledeljerteplasencia.es>
2024-10-22 20:58:12 +0200

    .

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-    require "views/utils/utils.php";
-    require "views/index.view.php";
-  
-?>
+require "views/utils/utils.php";
+require "entity/imagenGaleria.class.php";
+
+$arrayImagenes = array();
+
+for ($i = 0; $i < 12; $i++) {
+    $arrayImagenes[] = new imagenGaleria(($i + 1) . ".jpg", "Descripción imagen " . ($i + 1), rand(500, 1000), rand(100, 500), rand(0, 100));
+}
+require "views/index.view.php";
